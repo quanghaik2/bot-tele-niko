b7c6a60e5ad37e859e095e36aae0c0b7ff728f1a
quanghaik2 <chuquanghai2002@gmail.com>
2025-12-19 21:43:49 +0700

    update id

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const { CheckNikoUsersByDate } = require("./modules/checkNiko.js");
 
 const ID_CHAT = process.env.ID_CHAT;
 const RENDER_URL = process.env.RENDER_URL;
+sched;
+uler.scheduleNotify(bot, ID_CHAT);
 
 // const tokens = [
 //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjozMzAzLCJvaWQiOjIyMywicm9sZSI6InN0YWZmIiwiaWF0IjoxNzYyMzUzNjczLCJleHAiOjE3NjQ5NDU2NzN9.RzkxSvRZ4ZheYDqIX0NmrBMTOlFIPWiCMc-WUdb3fcc",
